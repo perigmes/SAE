582af5b0a54668e305cee60f5666e8da63d87b16
perigmes <96070235+perigmes@users.noreply.github.com>
2024-06-13 18:58:32 +0200

    fichiers

---
+++ Front-admin/src/pages/Products.jsx
@@ -0,0 +1,24 @@
+
+import { useState } from "react";
+import ProductsList from "./ProductsList"
+import ProductUpdate from "./ProductUpdate"
+import { observer } from "mobx-react";
+function Products() {
+    const [productSelect,setProductSelect]= useState(null);
+    return (
+        <>
+          <div className="dnc">
+          <div className="ari arr cex ddc ddo">
+            <ProductsList selectProduct={(value)=>{setProductSelect(value)}}/>
+            </div>
+            </div>
+            {productSelect &&    <div className="z as de md sn adk afj afv ari ase cex ddc diq">
+                <div className="ab pa adh ady afa afq afx bbe">
+                    <ProductUpdate productSelected={productSelect} validate={()=>setProductSelect(null)}/>
+                </div>
+            </div>}
+         
+        </>
+    )
+}
+export default observer(Products);
